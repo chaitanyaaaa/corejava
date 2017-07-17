1350f1eede676356ec25af62b9c86f33fc555799
chaitanyaaaa <chaitanyaaa@live.com>
2017-07-18 00:12:02 +0530

    1st commit for core java : prime numbers

---
+++ PrimeNumber.java
@@ -0,0 +1,27 @@
+package primeNumber;
+
+public class PrimeNumber 
+{
+	public static void prime_number (int lim)
+	{
+		int flag;
+		System.out.print("Prime numbers till "+ lim + " are: ");
+		for(int i=2; i<=lim; i++) 
+		{
+			flag=0;
+			for (int j=2; j<i; j++)
+			{
+				if(i%j==0)
+				{
+					flag = 1;
+				}
+//				System.out.println(i);
+			}
+			if(flag==0) 
+			{
+				System.out.print(i +" ");
+			}
+			
+		}
+    }
+}
